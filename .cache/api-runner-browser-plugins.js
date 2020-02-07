e4071f3eb5a67a47fce65f181abc2d1d494cbb96
Tyler Austin <tyler@webguyty.com>
2020-02-06 20:22:16 -0800

    Udated files to load store closed page correctly

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,7 +1,7 @@
 module.exports = [{
-      plugin: require('/Users/tyleraustin/Desktop/TND/node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui"},
     },{
-      plugin: require('/Users/tyleraustin/Desktop/TND/gatsby-browser.js'),
+      plugin: require('../gatsby-browser.js'),
       options: {"plugins":[]},
     }]
--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,10 +2,10 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/tyleraustin/Desktop/TND/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("/Users/tyleraustin/Desktop/TND/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("/Users/tyleraustin/Desktop/TND/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-index-main-js": () => import("/Users/tyleraustin/Desktop/TND/src/pages/indexMain.js" /* webpackChunkName: "component---src-pages-index-main-js" */),
-  "component---src-pages-page-2-js": () => import("/Users/tyleraustin/Desktop/TND/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+  "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-index-main-js": () => import("./../src/pages/indexMain.js" /* webpackChunkName: "component---src-pages-index-main-js" */),
+  "component---src-pages-page-2-js": () => import("./../src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
 }
 
