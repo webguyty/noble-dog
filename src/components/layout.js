37cd74b5de7b9b81d98a88aa91ff56895a1139bc
Tyler Austin <tyler@webguyty.com>
2020-02-20 18:32:13 -0800

    menu component name change

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import { useStaticQuery, graphql } from 'gatsby';
 
-// import Menu from './Menu';
+import Menu from './menu';
 import '../sass/main.scss';
 
 const Layout = ({ children }) => {
