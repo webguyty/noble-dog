3843e804d0d5240172f5f43750dc3f90bd6a2f24
Tyler Austin <tyler@webguyty.com>
2020-03-11 14:18:03 -0700

    commit before merge for homepage update

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -4,10 +4,10 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("./../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-contact-us-js": () => import("./../src/pages/contactUs.js" /* webpackChunkName: "component---src-pages-contact-us-js" */),
   "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-index-main-js": () => import("./../src/pages/indexMain.js" /* webpackChunkName: "component---src-pages-index-main-js" */),
   "component---src-pages-page-2-js": () => import("./../src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
-  "component---src-pages-training-info-js": () => import("./../src/pages/trainingInfo.js" /* webpackChunkName: "component---src-pages-training-info-js" */),
-  "component---src-pages-contact-us-js": () => import("./../src/pages/contactUs.js" /* webpackChunkName: "component---src-pages-contact-us-js" */)
+  "component---src-pages-training-info-js": () => import("./../src/pages/trainingInfo.js" /* webpackChunkName: "component---src-pages-training-info-js" */)
 }
 
