cfc4158da454f17c7b8ab021a5417d388560dd9b
Tyler Austin <tyler@webguyty.com>
2020-04-21 16:16:01 -0700

    added temporary closure div in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,6 +20,14 @@ const Layout = ({ children }) => {
   return (
     <>
       <Menu />
+      <div className='temp-closure'>
+        <h1>Temporary Closure</h1>
+        <p>
+          We are currently closed due to Coronavirus. Please check back soon,
+          hopefully we can get back to normal operations in the near future!
+          Stay safe.
+        </p>
+      </div>
       <main>
         <div className='content'>
           <main>{children}</main>
@@ -31,7 +39,7 @@ const Layout = ({ children }) => {
 };
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
 };
 
 export default Layout;
